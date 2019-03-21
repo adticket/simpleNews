cb71fea5c6347b552651c4ef8d80f48282333e12
cwr4 <christian.roeder@reservix.de>
2019-03-21 18:34:25 +0100

    Update of register form

--- views/User/register.php
+++ views/User/register.php
@@ -6,9 +6,27 @@
     <form method="post" action="register" class="form-horizontal">
         <div class="form-group">
             <label class="control-label">
-                Benutzername oder E-Mail-Adresse:
+                Vorname:
             </label>
-            <input type="text" name="login" class="form-control" required>
+            <input type="text" name="firstname" class="form-control" required>
+        </div>
+        <div class="form-group">
+            <label class="control-label">
+                Nachname:
+            </label>
+            <input type="text" name="surname" class="form-control" required>
+        </div>
+        <div class="form-group">
+            <label class="control-label">
+                Benutzername:
+            </label>
+            <input type="text" name="username" class="form-control" required>
+        </div>
+        <div class="form-group">
+            <label class="control-label">
+                E-Mail-Adresse:
+            </label>
+            <input type="email" name="email" class="form-control" required>
         </div>
         <div class="form-group">
             <label class="control-label">
@@ -16,6 +34,13 @@
             </label>
             <input type="password" name="password" class="form-control" required>
         </div>
+        <div class="form-group">
+            <label class="control-label">
+                Passwort:
+            </label>
+            <input type="password" name="password2" class="form-control" required>
+        </div>
+
         <div class="form-group">
             <input type="submit" class="btn btn-primary" value="Registrieren" />
         </div>
