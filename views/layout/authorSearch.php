6c99321bcd217b155ac244ed146b2aa465a0b3f2
cwr4 <christian.roeder@reservix.de>
2019-03-28 15:33:25 +0100

    Search via GET

--- views/layout/authorSearch.php
+++ views/layout/authorSearch.php
@@ -1,10 +1,10 @@
 <div class="container">
-    <form method="post" action="index">
+    <form method="get" action="index">
         <div class="form-inline justify-content-center">
             <select name="author" class="form-control mr-sm-2">
-                <option selected value="">Alle</option>
+                <option value="" selected>Alle</option>
                 <?php foreach($authors AS $author):?>
-                    <option value="<?php echo $author; ?>"<?php if(isset($_POST['author'])){ if(($_POST['author']==$author)) echo "selected"; }?>><?php echo $author; ?></option>
+                    <option value="<?php echo $author; ?>" <?php if(isset($_GET['author'])){ if(($_GET['author']==$author)) echo "selected"; }?>><?php echo $author; ?></option>
                 <?php endforeach; ?>
             </select>
             <button type="submit" class="btn btn-outline-secondary my-sm-0">Suchen</button>
