133509f67194098b80bcc6e3e5c7500ce2c300e5
cwr4 <christian.roeder@reservix.de>
2019-04-04 17:47:01 +0200

    update to all files in project folder;

--- init.php
+++ init.php
@@ -6,16 +6,19 @@
  * Time: 21:24
  */
 
-require __DIR__ . "/autoload.php";
+require __DIR__ . '/autoload.php';
 
 function e($string)
 {
     return htmlentities($string, ENT_QUOTES, 'UTF-8');
 }
 
+/*
+ *  Check if a string is empty
+ */
 function isEmpty($string)
 {
-    if(ctype_space($string) || $string=="")
+    if($string==='' || ctype_space($string))
     {
         return true;
     }
